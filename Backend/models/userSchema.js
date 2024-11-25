c4e896635fdb9e39a29c18f4c761629258433519
Pawas2005 <pawasranjan5@gmail.com>
2024-11-25 23:03:07 +0530

    Rebuild bcrypt dependency

--- Backend/models/userSchema.js
+++ Backend/models/userSchema.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import validator from "validator";
-
+const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
