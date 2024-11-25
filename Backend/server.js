7122da2ced1dd4c39df2199c259fcc014329a970
Pawas2005 <pawasranjan5@gmail.com>
2024-11-25 23:46:35 +0530

    apple

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import app from "./app.js";
 import cloudinary from "cloudinary";
-
+import cors from "cors";
+app.use(cors());
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
